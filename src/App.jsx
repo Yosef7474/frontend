19f0fa2caabbd4b08573c472b9c57c92e905fab7
Yosef <yosefdejene747@gmail.com>
2024-11-11 17:27:36 +0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Outlet } from "react-router-dom"
+import "./App.css"
 import Navbar from "./components/navbar"
+import Footer from "./components/footer"
+
 
 
 
@@ -12,7 +15,7 @@ function App() {
     <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 ">
       <Outlet/>
      </main>
-     <footer>footers</footer>
+     <Footer/>
     </>
   )
 }
